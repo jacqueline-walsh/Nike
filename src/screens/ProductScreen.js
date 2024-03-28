666996953c4e89b1cb4b9c51d4a48a1b8402c648
Jacqueline-Walsh <jacqueline.walsh34@gmail.com>
2024-03-28 12:18:00 +0000

    navigation

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Image, FlatList } from "react-native";
+import { StyleSheet, View, Image, FlatList, Pressable } from "react-native";
 import products from "../data/products";
+import { useNavigation } from "@react-navigation/native";
 
 const ProductScreen = () => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => (
-        <View style={styles.itemContainer}>
+        <Pressable style={styles.itemContainer} onPress={() => { navigation.navigate('Product Details') }}>
           <Image source={{ uri: item.image }} style={styles.image} />
-        </View>
+        </Pressable>
       )}
       numColumns={2}
     />
