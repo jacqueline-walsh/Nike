d186bd344dc8d540e5f7e1515984e2b97b2a47ce
Jacqueline-Walsh <jacqueline.walsh34@gmail.com>
2024-03-28 14:35:36 +0000

    setup redux and implement productSlice

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Navigation from './src/navigation';
+import { Provider } from 'react-redux';
+import { store } from './src/store/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Provider store={store}>
+      <View style={styles.container}>
 
-      <Navigation />
-      <StatusBar style="auto" />
-    </View>
+        <Navigation />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { StyleSheet, View, Image, FlatList, Pressable } from "react-native";
-import products from "../data/products";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector, useDispatch } from "react-redux";
+import { productSlice } from "../store/productSlice";
 
 const ProductScreen = () => {
+  const products = useSelector((state) => state.products.products);
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => (
-        <Pressable style={styles.itemContainer} onPress={() => { navigation.navigate('Product Details') }}>
+        <Pressable
+          style={styles.itemContainer}
+          onPress={() => {
+            // update selected product
+            dispatch(productSlice.actions.setSelectedProduct(item.id));
+            navigation.navigate("Product Details");
+          }}
+        >
           <Image source={{ uri: item.image }} style={styles.image} />
         </Pressable>
       )}
